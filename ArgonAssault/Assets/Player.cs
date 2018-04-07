bbb69dd19a463fd7ad41eaa03258608cb4f19a7f
Jon Woodall <jon@synsidmf.com>
2018-04-07 18:26:03 +0100

    Added Y-axis movement

--- ArgonAssault/Assets/Player.cs
+++ ArgonAssault/Assets/Player.cs
@@ -7,18 +7,30 @@ public class Player : MonoBehaviour {
 
     [Tooltip("In m^s-1")] [SerializeField] float xSpeed = 4f;
     [Tooltip("In m")] [SerializeField] float clampXMovement = 5f;
+    [Tooltip("In m^s-1")] [SerializeField] float ySpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float clampYMovement = 5f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        //Calculate new X Position
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
         float rawXPos = xOffset + transform.localPosition.x;
         float clampedXPos = Mathf.Clamp(rawXPos, -clampXMovement, clampXMovement);
-        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
-	}
+
+        //Calculate new Y Position
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float rawYPos = yOffset + transform.localPosition.y;
+        float clampedYPos = Mathf.Clamp(rawYPos, -clampYMovement, clampYMovement);
+
+        //Update Ship Position
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
 }
